5a09e5e30cd4786b4f85ff99c9a538097052d7eb
Pavlo Vovzhynskyy <pavlovovzhynskyy@MacBook-Air-Pavlo.local>
2022-11-08 11:02:48 +0100

    basics done need to add yargs

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-const {dateNow} = require('./date')
-global.testData = 'qwe'
-console.log(`get current date: ${dateNow()}`)
-console.log(global.testData)
+const contacts = require('./contacts');
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+	switch (action) {
+		case 'getAllContacts':
+			const allContacts = await contacts.getAllContacts();
+			console.log('qweqwe', allContacts);
+			break;
+		case 'getContactById':
+			const contactById = await contacts.getContactById(id);
+			console.log('byId', contactById);
+			break;
+		case 'addContact':
+			const addedContact = await contacts.addContact(name, email, phone);
+			console.log('addContact', addedContact);
+			break;
+		case 'removeContact':
+			const removedContact = await contacts.removeContact(id);
+			console.log('removeContact', removedContact);
+			break;
+
+		default:
+			console.log('Unknown action');
+			break;
+	}
+};
+
+invokeAction({
+	action: 'removeContact',
+	id:'3b66f2f0-dfeb-4ab0-ae19-ddd8f44058b9'
+});
+invokeAction({ action: 'getAllContacts' });
